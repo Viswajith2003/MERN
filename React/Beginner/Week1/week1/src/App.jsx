a0a68623f64000790b6afc49c297d31876bfc97e
Viswajith2003 <viswajithviswa715@gmail.com>
2025-04-02 12:16:44 +0530

    setText workging

--- React/Beginner/Week1/week1/src/App.jsx
+++ React/Beginner/Week1/week1/src/App.jsx
@@ -2,14 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count ,setCount]=useState(0)
+  // const [count ,setCount]=useState(0)
+  const[text,setText]=useState("")
+
+  const changeName=(e)=>
+  {
+    setText(e.target.value);
+  }
 
   return (
     <div>
-      <h1>Counter working:{count}</h1>
-      <button onClick={()=>{setCount(count+1)}}>Press Here</button>
+      {/* <h1>Counter working:{count}</h1>
+      <button onClick={()=>{setCount(count+1)}}>Press Here</button> */}
+
+      <input type="text" value={text} onChange={changeName}/>
+      <p>Entered text is:{text}</p>
+      <button onClick={()=>{setText("")}}>Reset</button>
+
     </div>
   )
 }
 
 export default App
+
+
