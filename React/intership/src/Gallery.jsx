d410f82b3562a0be9d939c33d78c3a8d32d62cf7
Viswajith2003 <viswajithviswa715@gmail.com>
2025-05-20 17:24:05 +0530

    useState set

--- React/intership/src/Gallery.jsx
+++ React/intership/src/Gallery.jsx
@@ -3,33 +3,44 @@ import { bookList } from "./assets/data";
 
 export default function Gallery() {
   const [index, setIndex] = useState(0);
+  const [showMore, setshowMore] = useState(false);
 
   const nextClick = () => {
-    setIndex(index + 1);
+    setIndex((index + 1) % bookList.length);
+  };
+
+  const prevClick = () => {
+    setIndex((index - 1 + bookList.length) % bookList.length);
   };
 
   const bookData = bookList[index];
   return (
     <div>
       <div style={{ display: "flex " }}>
-        <button
-          onClick={() => {
-            setIndex(index - 1);
-          }}
-        >
-          Prev
-        </button>
+        <button onClick={prevClick}>Prev</button>
         <button onClick={nextClick}>Next</button>
       </div>
       <h2>
         <i>{bookData.name} </i>
         by {bookData.artist}
       </h2>
-      <img src={bookData.url} alt={bookData.alt} />
       <h3>
         ({index + 1} of {bookList.length})
       </h3>
-      <p>{bookData.description}</p>
+      <button
+        onClick={() => {
+          setshowMore(!showMore);
+        }}
+      >
+        {showMore ? "showLess" : "showMore"}
+      </button>
+      {showMore && (
+        <>
+          <img src={bookData.url} alt={bookData.alt} />
+          <p>{bookData.description}</p>
+        </>
+      )}
     </div>
   );
 }
+
